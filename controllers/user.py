15afd4eafddfe22018ec09c8da3c28b6863ce7e3
Rob Kerry <rck504@york.ac.uk>
2015-03-03 16:55:35 +0000

    Added register

--- controllers/user.py
+++ controllers/user.py
@@ -1,8 +1,8 @@
 def login():
 	if current_user == None:
 		form = SQLFORM.factory(
-			 Field('username', length=128, default=''),
-			 Field('password', 'password', length=64)
+			Field('username', length=128, default=''),
+			Field('password', 'password', length=64)
 			)
 		if form.process().accepted:
 			users.login(form.vars.username, form.vars.password, db, session)
@@ -16,4 +16,13 @@ def logout():
 	redirect(URL(c='default'))
 
 def register():
-    return dict()
+	if current_user == None:
+		form = SQLFORM.factory(db.User, 
+			Field('cpassword', 'password', 
+			label="Confirm",
+			requires=IS_EQUAL_TO(request.vars.password, error_message='Passwords do not match.')),
+		    submit_button='Next >'
+		)
+		return dict(form=form)
+	else:
+		redirect(URL(c='default'))
