f14363dde6b8d643fbe50483319272a1e7621232
David Wotherspoon <david@dwotherspoon.com>
2015-03-03 13:30:16 +0000

    Track changes on modules

--- models/hook.py
+++ models/hook.py
@@ -1,5 +1,8 @@
 # Models are executed on every request. This file contains things we want
 # We can import our custom modules here, note that web2py caches modules.
+from gluon.custom_import import track_changes
+track_changes(True)
+
 import users
 import projects
 import documents
