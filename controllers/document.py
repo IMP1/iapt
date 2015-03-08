d0d8e89d0da1ae0d0a857ff5f6999d82c2b90f49
David Wotherspoon <david@dwotherspoon.com>
2015-03-08 18:26:15 +0000

    Document creation, remove uneeded module code

--- controllers/document.py
+++ controllers/document.py
@@ -3,7 +3,17 @@ def review():
 
 def new():
 	#doc = documents.create('New Document', projects.Project(request.args[0], db), '', db)
-	return dict(project=projects.Project(request.args[0], db))
+	project = projects.Project(request.args[0], db)
+
+	form = SQLFORM(db.Document, fields=['title', 'image'])
+	# Fill out excluded information
+	form.vars.project = project.getId()
+	form.vars.status = 1
+	# Customise form
+	form.custom.widget.title['_placeholder'] = 'Document Title'
+	if form.process().accepted:
+		redirect(URL(c='project', f='edit', args=[project.getId()]))
+	return dict(form=form, project=project)
 
 def transcribe():
 	pass
--- modules/documents.py
+++ modules/documents.py
@@ -2,20 +2,6 @@
 # ORM for Documents.
 import projects
 
-def create(title, project, image, db):
-	""" Create a new document and return document object.
-
-	Keyword arguments:
-	title -- The title for the new document.
-	project -- The parent project for the new  document.
-	image -- The image for the new document.
-	db -- Instance of db (DAL) in use.
-
-	"""
-	id = db.Document.insert(title=title, project=project.getId(),
-							 image=image, status=1)
-	return Document(id, db)
-
 class Document(object):
 	def __init__(self, id, db, get_project=True):
 		""" Constructor for documents.
