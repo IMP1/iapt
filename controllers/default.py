129d007fb3dbe1890270f611233f49f30f09f1b7
Rob Kerry <rck504@york.ac.uk>
2015-03-13 12:31:28 +0000

    Show recent documents on homepage closes #12
    Use new recent_documents function to return recent documents to view
    and then display them as a list of links which direct the user to
    either the transcribe or review page based on who owns the documents.

--- controllers/default.py
+++ controllers/default.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def index():
-	# Title the page and return the 9 most recent projects to the view
+	# Title the page and return the 9 most recent documents to the view
     response.title = "IAPT Group Six"
-    return dict(projects=projects.recent_projects(db))
+    return dict(recents=documents.recent_documents(db))
