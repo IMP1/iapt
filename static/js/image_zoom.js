2297a4d821c3512b8006fb8a0a880a2e85dfa0b6
IMP1 <huw_taylor@hotmail.co.uk>
2015-04-14 15:35:47 +0100

    fixed zoom bug

--- static/js/image_zoom.js
+++ static/js/image_zoom.js
@@ -4,16 +4,18 @@ var scaled = false;
 img.onclick = function(e) {
     if (scaled) {
         container.style.overflow = "";
+        container.style.maxHeight = "";
         img.style.cssText = "cursor: -moz-zoom-in; cursor: -webkit-zoom-in" 
         img.style.width = "100%";
     } else {
-        container.style.overflow = "scroll";
-        var x = (e.clientX - img.offsetLeft) / img.offsetWidth;
-        var y = (e.clientY - img.offsetTop) / img.offsetHeight;
-        container.scrollLeft = x * img.naturalWidth  - container.offsetWidth / 2;
-        container.scrollTop  = y * img.naturalHeight - container.offsetHeight / 2;
+        var x = (e.pageX - container.offsetLeft) / img.offsetWidth;
+        var y = (e.pageY - container.offsetTop)  / img.offsetHeight;
+        container.style.overflow = "auto";
+        container.style.maxHeight = "550px";
         img.style.cssText = "cursor: -moz-zoom-out; cursor: -webkit-zoom-out" 
         img.style.width = "" + img.naturalWidth + "px";
+        container.scrollLeft = x * img.naturalWidth  - container.offsetWidth / 2;
+        container.scrollTop  = y * img.naturalHeight - container.offsetHeight / 2;
     }
     scaled = !scaled;
 }
