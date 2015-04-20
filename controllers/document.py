404241c3259ee16a0ce8bbb7729433463d9a0bc3
Rob Kerry <rck504@york.ac.uk>
2015-04-20 14:33:30 +0100

    Add more comments #85

--- controllers/document.py
+++ controllers/document.py
@@ -2,11 +2,16 @@ import os
 import transcriptions
 
 def review():
+	# Require user to be logged in
 	auth_required('You must be logged in to review a document.')
+	# Get document
 	doc = documents.Document(request.args[0], db)
+	# Title the page
 	response.title = 'Review: ' + doc.getTitle()
+	# Redirect to view page if transcriptions have been accepted
 	if doc.isAccepted():
 		redirect(URL(c='document', f='view', args=[doc.getId()]))
+	# If user selects to accept transcriptions, accept them
 	if request.vars.action == "accept":
 		for section in doc.getProject().getSections():
 			# Accept chosen transcriptions
@@ -20,6 +25,7 @@ def review():
 		session.flash = {'msg': "You've accepted the following transcriptions and the document has been closed.",
 		                 'class': 'success_flash'}
 		redirect(URL(c='document', f='view', args=[doc.getId()]))
+	# If user selects to reject transcriptions, reject them
 	elif request.vars.action == "reject":
 		# Delete all the transcriptions
 		for section in doc.getProject().getSections():
@@ -33,14 +39,20 @@ def review():
 	return dict(document=doc)
 
 def view():
+	# Get document
 	doc = documents.Document(request.args[0], db)
+	# Title the page
 	response.title = 'View: ' + doc.getTitle()
+	# If transcriptions have not been reviewed and accepted, redirect to review page
 	if not doc.isAccepted():
 		redirect(URL(c='document', f='review', args=[doc.getId()]))
+	# Return the document to the view
 	return dict(document=doc)
 
 def transcribe():
+	# Get document
 	doc = documents.Document(request.args[0], db)
+	# Title the page
 	response.title = 'Transcribe: ' + doc.getTitle()
 	if request.env.request_method == 'POST':
 		# Check there is at least one transcription.
@@ -58,6 +70,7 @@ def transcribe():
 			session.flash = {'msg': "Thank you for transcribing '" + doc.getTitle() + "'",
 							 'class': 'success_flash'}
 			redirect(URL(c='project', f='view', args=doc.getProject().getId()))
+	# Return the document and project to the view
 	return dict(document=doc, project=doc.getProject())
 
 def image():
@@ -69,5 +82,7 @@ def image():
 	return response.stream(open(path, 'rb'),chunk_size=4096)
 
 def browse():
+	# Title page
     response.title = 'All Documents'
+    # Return all documents to the view
     return dict(docs=documents.search_results(db, ""))
